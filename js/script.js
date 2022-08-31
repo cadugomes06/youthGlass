09f5017eafbe9a227f1e5f42636dc6f089ddf4e4
Carlos Eduardo <cadu.010808@gmail.com>
2022-08-31 20:50:19 -0300

    javascript da sessão de perguntas na sessãode garantia

--- js/script.js
+++ js/script.js
@@ -22,3 +22,23 @@ function ativarProduto(parametros) {
 }
 
 parametros.forEach((ativarProduto))
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button")
+
+function ativarPergunta(event) {
+   const pergunta =  event.currentTarget
+   const controls = pergunta.getAttribute('aria-controls')
+   const resposta = document.getElementById(controls)
+
+   resposta.classList.toggle('ativo')
+   const ativo = resposta.classList.contains('ativo')
+   pergunta.setAttribute('aria-expanded', ativo)
+}
+
+function eventosPergunta(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPergunta)
